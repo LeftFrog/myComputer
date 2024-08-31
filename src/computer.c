83059837fbd55970878912189094bb903019460e
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-08-31 15:50:06 -0600

    Implemented memorySet function

--- src/computer.c
+++ src/computer.c
@@ -4,4 +4,13 @@ int memoryInit() {
     for (int i = 0; i < MEMORY_SIZE; i++) {
         memory[i] = 0;
     }
+    return 0;
+}
+
+int memorySet(uint8_t address, int16_t value) {
+    if (address > MEMORY_SIZE || address < 0) {
+        computerRegister |= MEMORY_OUT_OF_BOUNDS_FLAG;
+        return -1;
+    }
+    memory[address] = value;
 }
