22c7e55863a786634f176d57d7d46bed10f567fc
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-08-31 15:45:26 -0600

    Created register in the computer

--- include/computer.h
+++ include/computer.h
@@ -6,6 +6,14 @@
 
 int16_t memory[MEMORY_SIZE];
 
+uint8_t computerRegister;   // 8-bit register
+#define STACK_OVERFLOW_FLAG 0b00000001
+#define DIVISION_BY_ZERO_FLAG 0b00000010
+#define MEMORY_OUT_OF_BOUNDS_FLAG 0b00000100
+#define CLOCK_PULSE_IGNORED_FLAG 0b00001000
+#define INVALID_INSTRUCTION_FLAG 0b00010000
+
+
 int memoryInit();
 
 #endif //UNTITLED_LIBRARY_H
