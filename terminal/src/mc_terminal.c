b4240036c5bd7f92b420422df7bec55a025c1c43
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-09-05 22:14:41 -0600

    Added RETURN_HOME to the mc_clrscr function

--- terminal/src/mc_terminal.c
+++ terminal/src/mc_terminal.c
@@ -4,9 +4,11 @@
 #define ESC "\033"
 #define CSI ESC "["
 #define CLEAR_SCREEN CSI "2J"
+#define RETURN_CURSOR CSI "H"
 
 int mc_clrscr() {
     printf("%s", CLEAR_SCREEN);
+    printf("%s", RETURN_CURSOR);
     fflush(stdout);
     return 0;
 }
