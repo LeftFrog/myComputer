2724c10d46207252f9d0060a6f55c1354aec7605
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-09-05 22:02:23 -0600

    Added declarations of the terminal functions

--- terminal/include/mc_terminal.h
+++ terminal/include/mc_terminal.h
@@ -5,4 +5,21 @@
 #ifndef MC_TERMINAL_H
 #define MC_TERMINAL_H
 
+enum mc_colors {
+    MC_BLACK = 0,
+    MC_RED = 1,
+    MC_GREEN = 2,
+    MC_YELLOW = 3,
+    MC_BLUE = 4,
+    MC_MAGENTA = 5,
+    MC_CYAN = 6,
+    MC_WHITE = 7
+};
+
+int mc_clrscr();
+int mc_goto(int x, int y);
+int mc_getScreenSize(int *rows, int *cols);
+int mc_setForegroundColor(enum mc_colors color);
+int mc_setBackgroundColor(enum mc_colors color);
+
 #endif //MC_TERMINAL_H
