f936e778ff53b0b216efa70a3d9b81f5130384f1
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-09-05 22:13:15 -0600

    Implemented the mc_clrscr function

--- terminal/src/mc_terminal.c
+++ terminal/src/mc_terminal.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 #include "../include/mc_terminal.h"
+
+#define ESC "\033"
+#define CSI ESC "["
+#define CLEAR_SCREEN CSI "2J"
+
+int mc_clrscr() {
+    printf("%s", CLEAR_SCREEN);
+    fflush(stdout);
+    return 0;
+}
