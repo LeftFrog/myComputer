5d4a19fe7309e1f17c52e9964e71a21c101b61f8
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-09-05 22:33:53 -0600

    Implemented the mc_goto function

--- terminal/src/mc_terminal.c
+++ terminal/src/mc_terminal.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "../include/mc_terminal.h"
 
 #define ESC "\033"
@@ -10,5 +11,12 @@ int mc_clrscr() {
     printf("%s", CLEAR_SCREEN);
     printf("%s", RETURN_CURSOR);
     fflush(stdout);
+    return 0;
+}
+
+int mc_goto(int x, int y) {
+    char buffer[30];
+    int length = snprintf(buffer, sizeof(buffer), CSI "%d;%dH", y, x);
+    write(STDOUT_FILENO, buffer, length);
     return 0;
 }
