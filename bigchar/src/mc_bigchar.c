de2b5dd75aeff9c908d4bec9605db03960440a63
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-09-11 18:20:59 -0600

    Implemented mc_printA function

--- bigchar/src/mc_bigchar.c
+++ bigchar/src/mc_bigchar.c
@@ -1,3 +1,11 @@
 //
 // Created by Leonov Oleksandr on 9/11/24.
 //
+
+#include <stdio.h>
+#include "../include/mc_bigchar.h"
+
+int mc_printA(char *str) {
+    printf("%s\n", str);
+    return 0;
+}
