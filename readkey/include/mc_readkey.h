1c3039dc908e86a10b6f6080f67d7916cae9ec1f
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-11-19 12:21:35 -0700

    Declared main function in mc_readkey.h

--- readkey/include/mc_readkey.h
+++ readkey/include/mc_readkey.h
@@ -5,4 +5,13 @@
 #ifndef MC_READKEY_H
 #define MC_READKEY_H
 
+enum keys {
+
+};
+
+int mc_readkey(enum keys *);
+int mc_mytermsave();
+int mc_mytermrestore();
+int mc_mytermregime(int regime, int vtime, int vmin, int echo, int sigint);
+
 #endif //MC_READKEY_H
